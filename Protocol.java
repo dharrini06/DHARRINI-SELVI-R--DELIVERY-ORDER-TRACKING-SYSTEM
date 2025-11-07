71c657d4e8c4c364f1babca56ebd01b09d809dfd
dharrini06 <dharriniselvi06@gmail.com>
2025-11-07 13:58:23 +0530

    Create Protocol.java

---
+++ Protocol.java
@@ -0,0 +1,6 @@
+public class Protocol {
+    public static final String ADD_ORDER = "ADD_ORDER";
+    public static final String VIEW_ORDERS = "VIEW_ORDERS";
+    public static final String UPDATE_STATUS = "UPDATE_STATUS";
+    public static final String EXIT = "EXIT";
+}
